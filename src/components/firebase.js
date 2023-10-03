1396dcaa525a1ea43aa7453b93303ba8b07ca081
Dhruv Pandya <88615583+dhruvpandya1022@users.noreply.github.com>
2023-10-03 20:26:46 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,13 +4,7 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBaCndPIgQ-87J9eAGLHCQs19Iiv9UaWio",
-  authDomain: "chat-notification-d636d.firebaseapp.com",
-  projectId: "chat-notification-d636d",
-  storageBucket: "chat-notification-d636d.appspot.com",
-  messagingSenderId: "685383417299",
-  appId: "1:685383417299:web:b0aa7b79a9679a48fec326",
-  measurementId: "G-YXKJL44MNL"
+  
 };
 
 export const app = initializeApp(firebaseConfig);
